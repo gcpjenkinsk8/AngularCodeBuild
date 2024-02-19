ed88660fa2d8694b93dad46b785e90e4a6ffdcc3
gcpjenkinsk8 <gcpjenkinsk8@gmail.com>
2024-02-19 13:24:37 +0530

    new scripts

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Simple script to change permissions and restart services if needed
-chown -R ec2-user:ec2-user /home/dev/
-# restart services if needed, e.g., systemctl restart yourservice
-
